79d0665411818758b2c0dfbb6644f37bc4099dcc
Jon Sherrard <jon@andthats.it>
2018-01-17 22:54:16 +0000

    Fleshing out initial code

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 import React, { Component } from 'react'
+import firebase from 'firebase' 
 
 class ContentReactor extends Component {
-
+  constuctor(props) {
+    super(props) {
+      this.firebaseInstance = firebase.initializeApp(props.firebaseAppConfigk 'contentReactor')
+    }
+  }
+  
+  componentWillMount() {
+  	let messagesRef = fire.database().ref(`reactions/${this.props.contentIdentifier`)
+  }
 }
 
 export default ContentReactor
